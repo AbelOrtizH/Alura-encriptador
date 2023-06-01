751290aa38e724b6d3abd536f7b335cbd6eb74e0
Abel Ortíz H <hola@abelortizharo.com>
2023-06-01 00:10:38 -0500

    Falta la logica del encriptador.js estancado

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-// 1 Texto de ingreso
-// 2 reemplazar vocales, espacios
+//Encriptar 
+// 1 Texto de ingreso - Seleccionar valor del textarea
+// 2 Ocultar background img + informacion de mensaje encriptado
+// 3 ejecutar la funcion de encriptar texto
+// 4 devolver el texto encriptado al area de mensaje 
+// 5 mostrar el boton de copiar
+// 6 logica del boton copiar
+
+//------------------------------------------------
+//Desencriptar
+// 1 Seleccionar valor del textarea
+// 2 Ocultar background img + informacion de mensaje encriptado
+// 3 ejecutar la funcion de desencriptar texto
+// 4 devolver el texto desencriptado al area de mensaje 
+// 5 mostrar el boton de copiar
+// 6 logica del boton copiar
+
+//------------------------------------------------
+// Condiciones -- reemplazar vocales, espacios
 //La letra "e" es convertida para "enter"
 // La letra "i" es convertida para "imes"
 // La letra "a" es convertida para "ai"
@@ -9,28 +26,27 @@
 
 // Encriptado de texto
 function encriptar(){
-    
-    document.getElementById("warning-sms").style.display = "none";
-    
-    let encryptedText = document.getElementById("encrypted-sms").innerHTML;
+    // let encriptarTexto = document.getElementsByClassName("textarea").value;
+    // let textoEncriptado = encriptarMensaje(encriptarTexto);
+    document.getElementsByClassName("mensaje").style.display = "none";
 
-    let mapObjs = {
-        a:"ai",
-        e:"enter",
-        i:"imes",
-        o:"ober",
-        u:"ufat"
-    };
 
-    let newEncryptedText = encryptedText.replace(/a|e|i|o|u/g, function(matched) {
-            return mapObjs[matched]
-    });
-
-    return newEncryptedText;
+    // function encriptarMensaje(texto) {
+    //     let texto = texto
+    //         .replace('/a/g', 'ai')
+    //         .replace('/e/g', 'enter')
+    //         .replace('/i/g','imes')
+    //         .replace('/o/g', 'over')
+    //         .treplace('/u/g', 'ufat');
+        
+    //     return texto;
+    // };
 
 };
 
-console.log(encriptar());
+
+
+  
 
 
 
