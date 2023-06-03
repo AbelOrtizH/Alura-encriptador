c341f10ca700475f7ba0b3a37ce69fcb32be8bcd
Abel Ortiz H <hola@abelortizharo.com>
2023-06-02 22:57:10 -0500

    logica -> .replace parametros, btn encriptador + desencriptar + copiar  + css desktop

--- script.js
+++ script.js
@@ -5,7 +5,14 @@
 // 4 devolver el texto encriptado al area de mensaje 
 // 5 mostrar el boton de copiar
 // 6 logica del boton copiar
-
+//------------------------------------------------
+// Condiciones -- reemplazar vocales, espacios
+//La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+// 3 retornar el valor reemplazado
 //------------------------------------------------
 //Desencriptar
 // 1 Seleccionar valor del textarea
@@ -15,39 +22,41 @@
 // 5 mostrar el boton de copiar
 // 6 logica del boton copiar
 
-//------------------------------------------------
-// Condiciones -- reemplazar vocales, espacios
-//La letra "e" es convertida para "enter"
-// La letra "i" es convertida para "imes"
-// La letra "a" es convertida para "ai"
-// La letra "o" es convertida para "ober"
-// La letra "u" es convertida para "ufat"
-// 3 retornar el valor reemplazado
-
 // Encriptado de texto
+
 function encriptar(){
-    // let encriptarTexto = document.getElementsByClassName("textarea").value;
-    // let textoEncriptado = encriptarMensaje(encriptarTexto);
-    document.getElementsByClassName("mensaje").style.display = "none";
+    let frase = document.querySelector(".textarea").value.toLowerCase();
+    //i -> tome en cuenta mayusculas y minisculas
+    //g -> tome toda la linea o la oracion
+    //m -> tome encuenta multiples lineas
+    var textoEncriptado = frase.replace(/e/img, "enter");
+    var textoEncriptado = textoEncriptado.replace(/o/img, "ober");
+    var textoEncriptado = textoEncriptado.replace(/i/img, "imes");
+    var textoEncriptado = textoEncriptado.replace(/a/img, "ai");
+    var textoEncriptado = textoEncriptado.replace(/u/img, "ufat");
 
-
-    // function encriptarMensaje(texto) {
-    //     let texto = texto
-    //         .replace('/a/g', 'ai')
-    //         .replace('/e/g', 'enter')
-    //         .replace('/i/g','imes')
-    //         .replace('/o/g', 'over')
-    //         .treplace('/u/g', 'ufat');
-        
-    //     return texto;
-    // };
-
+    document.querySelector(".mensaje").innerHTML = textoEncriptado;
 };
 
 
+function desencriptar(){
+    let frase = document.querySelector(".textarea").value.toLowerCase();
+    //i -> tome en cuenta mayusculas y minisculas
+    //g -> tome toda la linea o la oracion
+    //m -> tome encuenta multiples lineas
+    var textoEncriptado = frase.replace(/enter/img, "e");
+    var textoEncriptado = textoEncriptado.replace(/ober/img, "o");
+    var textoEncriptado = textoEncriptado.replace(/imes/img, "i");
+    var textoEncriptado = textoEncriptado.replace(/ai/img, "a");
+    var textoEncriptado = textoEncriptado.replace(/ufat/img, "u");
 
-  
+    document.querySelector(".mensaje").innerHTML = textoEncriptado;
+};
 
-
+function copiar(){
+    let contenido = document.querySelector(".mensaje");
+    contenido.select();
+    document.execCommand("copy")
+}
 
 
